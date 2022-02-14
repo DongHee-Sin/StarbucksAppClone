e370fdd17b3739223e9b3d428e07c5c56c3cc606
DongHee-Sin <voslr0601@naver.com>
2022-02-15 00:11:03 +0900

    이미지 추가 상단 View 작업중

---
+++ StarbucksApp/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  StarbucksApp
+//
+//  Created by 신동희 on 2022/02/13.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
