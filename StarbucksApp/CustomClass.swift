4f78c1be84714b128e6ded40e39c3c5eb78c9bbd
DongHee-Sin <voslr0601@naver.com>
2022-02-15 14:36:19 +0900

    메인 View 완성

---
+++ StarbucksApp/CustomClass.swift
@@ -0,0 +1,68 @@
+//
+//  CustomClass.swift
+//  StarbucksApp
+//
+//  Created by 신동희 on 2022/02/15.
+//
+
+import Foundation
+import UIKit
+
+// UILabel 자간
+open class CustomLabel : UILabel {
+    @IBInspectable open var characterSpacing:CGFloat = 1 {
+        didSet {
+            let attributedString = NSMutableAttributedString(string: self.text!)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: self.characterSpacing, range: NSRange(location: 0, length: attributedString.length))
+            self.attributedText = attributedString
+        }
+
+    }
+}
+
+
+// UILabel 위쪽 정렬
+open class VerticalAlignLabel: UILabel {
+    enum VerticalAlignment {
+        case top
+        case middle
+        case bottom
+    }
+
+    var verticalAlignment : VerticalAlignment = .top {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override public func textRect(forBounds bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
+        let rect = super.textRect(forBounds: bounds, limitedToNumberOfLines: limitedToNumberOfLines)
+
+        if UIView.userInterfaceLayoutDirection(for: .unspecified) == .rightToLeft {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        } else {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        }
+    }
+
+    override public func drawText(in rect: CGRect) {
+        let r = self.textRect(forBounds: rect, limitedToNumberOfLines: self.numberOfLines)
+        super.drawText(in: r)
+    }
+    
+}
+
