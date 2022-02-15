a14d46608413419de249334ca81fba223c94b175
DongHee-Sin <voslr0601@naver.com>
2022-02-15 19:39:19 +0900

    상품 상세 페이지에 주문 버튼 추가

--- StarbucksApp/ProductInfoViewController.swift
+++ StarbucksApp/ProductInfoViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ProductInfoViewController: UIViewController {
     
+    // 핫/아이스 토글
     @IBOutlet weak var hotOrIced: UISegmentedControl!
+    
+    // 주문하기 버튼
+    @IBOutlet weak var orderButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Segmented Control 세팅
+        
+        // Segmented Control 설정
         hotOrIced.backgroundColor = .white
         hotOrIced.selectedSegmentTintColor = .orange
         
+        
+        // 주문버튼 테두리 설정
+        orderButton.layer.cornerRadius = 15
     }
 }
