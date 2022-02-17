943c4976c4b62dea32f94df2568af3b9456f8968
DongHee-Sin <voslr0601@naver.com>
2022-02-17 12:53:50 +0900

    1. orderView의 주문하기 버튼과 paymentView 연결 2. Delegate 사용하여 paymentView로 상품 정보 전달

--- StarbucksApp/ViewController/PaymentViewController.swift
+++ StarbucksApp/ViewController/PaymentViewController.swift
@@ -9,9 +9,43 @@ import UIKit
 
 class PaymentViewController: MainViewController {
     
+    // delegate
     var delegate: ProductDataDelegate?
+    
+    
+    // dismissButton
+    @IBAction func didDismissButtonTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // UI 연결
+    @IBOutlet weak var paymentButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRadiusToUIButton(paymentButton, size: 20)
+        
+        
+        // MARK: - UI 업데이트
+        
+        // orderVC 가져오기
+        guard let orderVC = delegate as? OrderViewController else {
+            return
+        }
+        
+        // productInfoVC 가져오기
+        guard let productVC = orderVC.delegate as? ProductInfoViewController else {
+            return
+        }
+        
+        // 주문할 제품 이름 가져오기
+        guard let toPaymentProductName = productVC.delegate?.getProductName() else {
+            return
+        }
+        
+        
     }
 }
