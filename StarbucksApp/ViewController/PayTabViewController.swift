c7d79a9b614af9c111c25a27bff1bb968a9d3762
DongHee-Sin <voslr0601@naver.com>
2022-02-18 14:42:05 +0900

    TabBar - Pay Scene 완성

--- StarbucksApp/ViewController/PayTabViewController.swift
+++ StarbucksApp/ViewController/PayTabViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class PayTabViewController: MainViewController {
+    
+    @IBOutlet weak var cardInfoView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addShadow(cardInfoView, color: UIColor.lightGray.cgColor, width: 0, height: 10, alpha: 0.7, radius: 10)
     }
 }
